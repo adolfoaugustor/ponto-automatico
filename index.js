36c71215a0fd883a3aaada50265e3c65c65c7012
Adolfo A. R <adolfo.rodrigues@cnj.jus.br>
2022-12-15 08:09:02 -0300

    added notification for linux terminal.

--- index.js
+++ index.js
@@ -2,56 +2,42 @@ const cron = require('node-cron');
 const EventEmitter = require('events');
 const ponto = require('./requestPoint.js');
 const event = new EventEmitter();
+const notifier = require('node-notifier');
 
-console.log('Cron ponto Basis...');
-
-// Make your callback function asynchronous so that you can use await
+console.log('________Registro de ponto: trabalhar_______');
 async function doSomething() {
-    console.log('Iniciando...');
-    console.log(new Date().toLocaleString());
+    console.log(new Date().toLocaleString() + ': __Hora__');
     return ponto.baterPonto();
 }
 
-const task1 = cron.schedule('1 10 9 * * *', async () => {
-    console.log('Entrada 1!');
+const task1 = cron.schedule('1 55 08 * * *', async () => {
+    console.log('__________Entrada 1 Bora Codar__________');
     let st = await doSomething();
-    event.emit('Ponto 1 registrado... '+` ${st}`);
+    notifier.notify('__________Entrada 1 Bora Codar__________ '+st);
+    event.emit(`Ponto 1 registrado... ${st}`);
 });
-// execução de entrada 1 completada
-event.on('Cron de entrada completo', () => {
-    console.log('Finalizado!');
-    task1.stop();
-});
+event.on('Cron de entrada completo', () => {task1.stop()});
 
-const task2 = cron.schedule('2 00 12 * * *', async () => {
-    console.log('Saída 1!');
+const task2 = cron.schedule('2 01 12 * * *', async () => {
+    console.log('----------Saída 1 Almoço----------');
     let st = await doSomething();
-    event.emit('Saída 1 registrado... '+` ${st}`);
+    notifier.notify('----------Saída 1 Almoço---------- '+st);
+    event.emit(`Saída 1 registrado... ${st}`);
 });
-// JOB COMPLETED exit 2 event is emitted and stop the task2
-event.on('Cron de Saída completo', () => {
-    console.log('Finalizado...');
-    task2.stop();
-});
+event.on('Cron de Saída completo', () => {task2.stop()});
 
-const task3 = cron.schedule('2 0 13 * * *', async () => {
-    console.log('Entrada 2');
+const task3 = cron.schedule('2 01 13 * * *', async () => {
+    console.log('__________Entrada 2 Voltando___________');
     let st = await doSomething();
-    event.emit('Entrada 2 registrado... '+` ${st}`);
+    notifier.notify('__________Entrada 2 Voltando___________ '+st);
+    event.emit(`Entrada 2 registrado... ${st}`);
 });
-// JOB COMPLETED Start 2 event is emitted and stop the task3
-event.on('Cron de Entrada completo 2', () => {
-    console.log('Finalizado!');
-    task3.stop();
-});
+event.on('Cron de Entrada completo 2', () => {task3.stop()});
 
-const task4 = cron.schedule('1 10 18 * * *', async () => {
-    console.log('Saída 2');
+const task4 = cron.schedule('1 55 17 * * *', async () => {
+    console.log('----------Saída 2 Vlw Tchau----------');
     let st = await doSomething();
-    event.emit('Saída 2 registrado... '+` ${st}`);
+    notifier.notify('----------Saída 2 Vlw Tchau---------- '+st);
+    event.emit(`Saída 2 registrado... ${st}`);
 });
-// JOB COMPLETED exit 4 event is emitted and stop the task3
-event.on('Cron de Saída completo', () => {
-    console.log('Finalizado!');
-    task4.stop();
-});
+event.on('Cron de Saída completo', () => {task4.stop()});
--- requestPoint.js
+++ requestPoint.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const axios = require("axios");
 
+
 module.exports.baterPonto = async (req, res) => {
   let data = JSON.stringify({
     "atividadeId": null,
